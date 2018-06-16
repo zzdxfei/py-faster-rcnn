fdeb46e2ffc85ff170cd22b49ff879f2ccbb10d8
zzdxfei <zzdxfei@163.com>
2018-06-16 11:15:23 +0800

    read proposal_layer.py

--- lib/rpn/proposal_layer.py
+++ lib/rpn/proposal_layer.py
@@ -23,6 +23,7 @@ class ProposalLayer(caffe.Layer):
     transformations to a set of regular boxes (called "anchors").
 
     输出ROI blob，也就是Fast-rcnn中的输入之一．
+    top[0]为(n, x1, y1, x2, y2)，top[1]为对应的Roi的score
     """
 
     def setup(self, bottom, top):
@@ -50,6 +51,13 @@ class ProposalLayer(caffe.Layer):
 
     def forward(self, bottom, top):
         """
+        bottom[0] 预测的所有anchor的得分，前9个通道为是bg的概率，后面为目标的概率
+        bottom[1] 预测的是每个anchor对应的包围盒的偏移
+        bottom[2] 为网络的图片的信息
+
+        top[0] 为(n, x1, y1, x2, y2)
+        top[1] 对应的得分
+
         1. 获取所有的anchors
         2. 对所有的anchors进行包围盒变换
         3. 选取符合要求的anchors进行输出
@@ -76,10 +84,13 @@ class ProposalLayer(caffe.Layer):
         nms_thresh    = cfg[cfg_key].RPN_NMS_THRESH
         min_size      = cfg[cfg_key].RPN_MIN_SIZE
 
+        # 前９个是box是bg的概率，后9个为是目标的概率
         # the first set of _num_anchors channels are bg probs
         # the second set are the fg probs, which we want
+        # 取出每个box是目标的概率
         scores = bottom[0].data[:, self._num_anchors:, :, :]
         bbox_deltas = bottom[1].data
+        # 网络中图片的大小以及相对于原始图片的放缩比例
         im_info = bottom[2].data[0, :]
 
         if DEBUG:
@@ -87,18 +98,23 @@ class ProposalLayer(caffe.Layer):
             print 'scale: {}'.format(im_info[2])
 
         # 1. Generate proposals from bbox deltas and shifted anchors
+        # 特征图的高度和宽度
         height, width = scores.shape[-2:]
 
         if DEBUG:
             print 'score map size: {}'.format(scores.shape)
 
         # Enumerate all shifts
+        # 对于一个h * w的图，将其所有可能的位置列出
+        # _feat_stride指的是特征图上一个像素，对应于原始图片上16个像素
         shift_x = np.arange(0, width) * self._feat_stride
         shift_y = np.arange(0, height) * self._feat_stride
         shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+        # 对应于(x, y, w, h),转置后为的形状为(w * h, 4)
         shifts = np.vstack((shift_x.ravel(), shift_y.ravel(),
                             shift_x.ravel(), shift_y.ravel())).transpose()
 
+        # anchors相对于网络输入图片的
         # Enumerate all shifted anchors:
         #
         # add A anchors (1, A, 4) to
@@ -108,7 +124,7 @@ class ProposalLayer(caffe.Layer):
         A = self._num_anchors
         K = shifts.shape[0]  # anchor的位置个数
         # anchors shape is (K, A, 4) 
-        anchors = self._anchors.reshape((1, A, 4)) + \
+        anchors = self._anchors.reshape((1, A, 4)) +
                   shifts.reshape((1, K, 4)).transpose((1, 0, 2))
         anchors = anchors.reshape((K * A, 4))
 
@@ -119,6 +135,7 @@ class ProposalLayer(caffe.Layer):
         # transpose to (1, H, W, 4 * A)
         # reshape to (1 * H * W * A, 4) where rows are ordered by (h, w, a)
         # in slowest to fastest order
+        # 将bbox和anchors对应起来
         bbox_deltas = bbox_deltas.transpose((0, 2, 3, 1)).reshape((-1, 4))
 
         # Same story for the scores:
@@ -126,9 +143,11 @@ class ProposalLayer(caffe.Layer):
         # scores are (1, A, H, W) format
         # transpose to (1, H, W, A)
         # reshape to (1 * H * W * A, 1) where rows are ordered by (h, w, a)
+        # 将scores和anchors对应起来
         scores = scores.transpose((0, 2, 3, 1)).reshape((-1, 1))
 
         # Convert anchors into proposals via bbox transformations
+        # 对anchors进行调整，得到预测的所有proposals，proposals是相对于网络输入图片的
         proposals = bbox_transform_inv(anchors, bbox_deltas)
 
         # 2. clip predicted boxes to image
